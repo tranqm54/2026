b0620f1001c80b9f3d9950b741d79568dd84fda2
tranqm <tranqm54@gmail.com>
2026-01-09 11:19:42 +0700

    update js

--- src/backend/renderUI.js
+++ src/backend/renderUI.js
@@ -36,33 +36,76 @@ function init_header(){
 }
 
 function onclick_header(key){
-    console.log(key)
+    set_layout(key);
     // translator.setMode(key);
     // translator.updateUI();
 }
 
 // layout create
 function create_layout_intro(main){
-    main.className = "content-card";
+    const image_list = ["images/thumb1.jpg", "images/thumb2.jpg", "images/thumb3.jpg", "images/thumb4.jpg"];
 
+    // div index 0
     const div = document.createElement("div");
     div.className = "intro-text";
 
-    const h2 = document.createElement("h2");
-    h2.textContent = translator.get("nav_intro_general_introduction");
+        const h2 = document.createElement("h2");
+        h2.textContent = translator.get("nav_intro_general_introduction");
+        div.appendChild(h2);
 
-    const p = document.createElement("p");
-    p.textContent = translator.get("nav_intro_content");
+        const p = document.createElement("p");
+        p.textContent = translator.get("nav_intro_content");
+        div.appendChild(p);
 
-    div.appendChild(h2);
-    div.appendChild(p);
-
+    
     main.appendChild(div)
+
+
+    const div1 = document.createElement("div");
+    div1.className = "image-side";
+        // div index 1 1
+        const div1_div = document.createElement("div");
+        div1_div.className = "main-photo";
+
+        const img = document.createElement("img");
+        img.src = "images/family.jpg";
+        img.alt = "Gia đình sum vầy";
+        div1_div.appendChild(img);
+
+        div1.appendChild(div1_div);
+        // div index 1 2
+        const div1_div2 = document.createElement("div");
+        div1_div2.className = "thumb-gallery";
+
+        for (let item of image_list){
+            const img = document.createElement("img");
+            img.src = item;
+            div1_div2.appendChild(img);
+        }
+
+        div1.appendChild(div1_div2);
+
+    main.appendChild(div1);
+
+
+    document.body.appendChild(main);
+}
+
+function create_layout_customs(main){
+    main.innerHTML = "Phong tục";
+}
+
+function create_layout_cuisine(main){
+    main.innerHTML = "Ẩm thực";
+}
+
+function create_layout_significance(main){
+    main.innerHTML = "Ý nghĩa";
 }
 
 
 // main
-function set_layout(key){
+function set_layout(key = "nav_intro"){
     let main = document.querySelector("main");
 
     if (main){
@@ -71,13 +114,15 @@ function set_layout(key){
         main = document.createElement("main");
         main.className = "content-card";
         document.body.appendChild(main);
+
     }
 
 
     const layouts = {
         "nav_intro": () => create_layout_intro(main),
         "nav_customs": () => create_layout_customs(main),
-        "nav_cuisine": () => create_layout_cuisine(main)
+        "nav_cuisine": () => create_layout_cuisine(main),   
+        "nav_significance": () => create_layout_significance(main),
     };
 
     const renderFunc = layouts[key];
